260d8a15cc8a053dbf7f399286e789b456e965e2
Bryam Talledo <bryam.talledo@outlook.com>
2025-03-20 11:03:12 -0500

    Add UrlResponse

---
+++ src/main/java/com/bjtg/shortly/dto/url/UrlResponse.java
@@ -0,0 +1,32 @@
+package com.bjtg.shortly.dto.url;
+
+public class UrlResponse {
+
+    private String originalUrl;
+    private String shortCode;
+
+    public UrlResponse() {
+    }
+
+    public UrlResponse(String shortCode, String originalUrl) {
+        this.shortCode = shortCode;
+        this.originalUrl = originalUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+}
