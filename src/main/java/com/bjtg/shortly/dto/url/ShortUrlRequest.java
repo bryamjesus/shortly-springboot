3d54ed03d7eb7104650aead5c095168476d925f5
Bryam Talledo <bryam.talledo@outlook.com>
2025-03-22 16:16:55 -0500

    Add new validation a ShortUrlRequest

--- src/main/java/com/bjtg/shortly/dto/url/ShortUrlRequest.java
+++ src/main/java/com/bjtg/shortly/dto/url/ShortUrlRequest.java
@@ -1,10 +1,12 @@
 package com.bjtg.shortly.dto.url;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public class ShortUrlRequest {
 
-    @NotBlank(message = "url requerido")
+    @NotBlank(message = "La URL es requerida")
+    @URL(message = "Debe ser una URL válida")
     private String url;
 
     public ShortUrlRequest() {
