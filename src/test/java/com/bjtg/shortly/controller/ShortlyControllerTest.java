d82c8545371f8fb5d5c9d5ac4289d2279d5a72f7
Bryam Talledo <bryam.talledo@outlook.com>
2025-04-15 11:39:10 -0500

    Add dependecies project

---
+++ src/test/java/com/bjtg/shortly/controller/ShortlyControllerTest.java
@@ -0,0 +1,44 @@
+package com.bjtg.shortly.controller;
+
+import com.bjtg.shortly.dto.url.UrlResponse;
+import com.bjtg.shortly.service.UrlManagerService;
+import com.bjtg.shortly.util.ResponseUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ShortlyController.class)
+public class ShortlyControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UrlManagerService urlManagerService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testGetUrlByCodeUrl() throws Exception {
+        String codeUrl = "abc12345";
+        UrlResponse mockResponse = new UrlResponse();
+
+        when(urlManagerService.getUrlByCode(codeUrl)).thenReturn(mockResponse);
+
+        mockMvc.perform(get("/shortly/{codeUrl}", codeUrl))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(
+                        ResponseUtil.succes("Url retrieved successfully", mockResponse)
+                )));
+    }
+}
---
+++ src/test/java/com/bjtg/shortly/service/UrlShortServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.bjtg.shortly.service;
+
+import com.bjtg.shortly.service.impl.UrlShortServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UrlShortServiceImplTest {
+    private final UrlShortServiceImpl urlShortService = new UrlShortServiceImpl();
+
+    @Test
+    @DisplayName("Generar código")
+    public void testShortUrl_Generates8CharacterCode() {
+        String code = urlShortService.shortUrl();
+        System.out.println("code = " + code);
+
+        // Verificar que no sea nulo
+        assertNotNull(code, "El código generado no debe ser nulo");
+
+        // Verificar que tenga exactamente 8 caracteres
+        assertEquals(8, code.length(), "El código debe tener 8 caracteres");
+
+        // Verificar que solo contenga letras (mayúsculas o minúsculas) y números
+        assertTrue(code.matches("[a-zA-Z0-9]{8}"), "El código debe contener solo letras y números");
+    }
+
+    @Test
+    @DisplayName("Generar diferentes códigos")
+    public void testShortUrl_GeneratesDifferentCodes() {
+        String code1 = urlShortService.shortUrl();
+        String code2 = urlShortService.shortUrl();
+        System.out.println("code2+ code1 = " + code2 + code1);
+
+        // Aunque la probabilidad de colisión es muy baja, se espera que en dos llamadas consecutivas sean diferentes.
+        assertNotEquals(code1, code2, "Dos códigos generados deberían ser diferentes");
+    }
+}
